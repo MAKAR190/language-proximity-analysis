04c79d117eef083842af712337496aacc436ff07
MAKAR190 <lutskyimakar@gmail.com>
2025-11-06 15:03:50 +0100

    feat(scrapper): made first verison of scrapper, now it scraps 10 topics with MAX_WORDS of 100 per topic; TODO: add 10 more topics to config

--- python/modules/scraper.py
+++ python/modules/scraper.py
@@ -6,9 +6,10 @@
 # ├─ processor: IProcessor → TextProcessor
 # └─ storage: IStorage → JSONStorage / DatabaseStorage
 
-import json, requests, os
+import json, requests, os, lxml
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from bs4 import BeautifulSoup
 
 # Types and Interfaces
 @dataclass
@@ -47,25 +48,59 @@ class IStorage(ABC):
         pass
 # -------------------------
 
-class WikipediaFetcher(IFetcher):
+class PageFetcher(IFetcher):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                      "AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/120.0.0.0 Safari/537.36"
+    }
     def fetch(self, topic: Topic) -> str:
-        pass
+        page = requests.get(topic.url, headers=self.headers, allow_redirects=True)
+        return page.text
 
-class WikipediaParser(IParser):
-    def parse(self, raw_data: str) -> list[str]:
-        pass
+class PageParser(IParser):
+    MAX_WORDS = 100
+    def parse(self, html: str) -> list[str]:
+        soup = BeautifulSoup(html, "lxml")
+        words = []
+        for li in soup.select("span.term a"):
+            if len(words) >= self.MAX_WORDS:
+                 break
+            text = li.get_text(strip=True)
+            if text and len(text.split()) == 1:
+                words.append(text)
+
+        return words
 
 class TextProcessor(IProcessor):
     def process(self, words: list[str]) -> list[str]:
-        pass
+        return sorted(set(w.lower() for w in words))
 
-class JSONStorage(IStorage):
+class TextStorage(IStorage):
+    ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+    DATA_DIR = os.path.join(ROOT_DIR, "data")
+    def __init__(self):
+        self._file_initialized = False
+
     def store(self, topic: Topic, words: list[str]) -> None:
-        pass
+        os.makedirs(self.DATA_DIR, exist_ok=True)
+        path = os.path.join(self.DATA_DIR, "scrapped.txt")
+        mode = "a+" if self._file_initialized else "w+"
+        self._file_initialized = True
+
+        with open(path, mode, encoding="utf-8") as scrapped_file:
+            scrapped_file.write(f"Topic: {topic.title}\n{words}\n\n")
 
 class Scraper:
-    def __init__(self, topics: list[Topic]):
+    def __init__(self, topics: list[Topic],  fetcher: IFetcher,
+                 parser: IParser,
+                 processor: IProcessor,
+                 storage: IStorage):
         self.topics = TopicsInput(topics)
+        self.fetcher = fetcher
+        self.parser = parser
+        self.processor = processor
+        self.storage = storage
         self._output: list[TopicOutput] = []
 
     @property
@@ -78,7 +113,12 @@ class Scraper:
         self._output = value
 
     def run(self):
-        pass
+        for t in self.topics:
+            html = self.fetcher.fetch(t)
+            words = self.parser.parse(html)
+            processed_words = self.processor.process(words)
+            self.storage.store(t, processed_words)
+            self.output.append(TopicOutput(topic=t.title, words=processed_words))
 
 if __name__ == "__main__":
     # Importing topics config for independent single module testing
@@ -86,9 +126,9 @@ if __name__ == "__main__":
         data = json.load(f)
 
     try:
-        config = data["topics"]
+        config_topics = [Topic(url=item["url"], title=item["title"]) for item in data["topics"]]
     except KeyError:
         raise KeyError("Missing 'topics' key in config.json")
 
-    scraper = Scraper(config)
+    scraper = Scraper(config_topics, fetcher=PageFetcher(), parser=PageParser(), processor=TextProcessor(), storage=TextStorage())
     scraper.run()
--- python/pipeline.py
+++ python/pipeline.py
@@ -1,9 +1,1 @@
 # Main file to call all modules
-
-scraper = Scraper()
-
-scraped_data = scraper.scrap()
-
-translator = Translator()
-
-data = translator.translate(scraped_data)
